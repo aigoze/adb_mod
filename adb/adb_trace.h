d539e9787874ef2bb4c9c6850cabdf061dbbdef9
Jiandong Huang <huangjiandong@haloai.com>
2017-07-17 19:14:01 +0800

    trace adb by trace mask

--- adb/adb_trace.h
+++ adb/adb_trace.h
@@ -81,8 +81,8 @@ extern int     adb_trace_mask;
 extern unsigned char    adb_trace_output_count;
 void    adb_trace_init(void);
 
-//#  define ADB_TRACING  ((adb_trace_mask & (1 << TRACE_TAG)) != 0)
-#  define ADB_TRACING  1    //HJD
+#  define ADB_TRACING  ((adb_trace_mask & (1 << TRACE_TAG)) != 0)
+//#  define ADB_TRACING  1    //HJD
 /* you must define TRACE_TAG before using this macro */
 #if ADB_HOST
 #  define  D(...)                                      \
