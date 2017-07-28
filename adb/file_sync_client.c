780a4feb5dc4bfffbb9ac9e30b98dbb59e700a15
Jiandong Huang <huangjiandong@haloai.com>
2017-07-28 21:45:57 +0800

    compiler issues " ; "

--- adb/file_sync_client.c
+++ adb/file_sync_client.c
@@ -1139,7 +1139,7 @@ int do_halo_pull(const char *rpath, const char *lpath, int show_progress, int co
         p_sync_param.lpath = lpath;
         p_sync_param.show_progress = show_progress;
         printf("======creating halo_sync_recv thread\n");
-        err = pthread_create(&t_recv, &attr, (void*)halo_sync_recv, (void*)&p_sync_param)
+        err = pthread_create(&t_recv, &attr, (void*)halo_sync_recv, (void*)&p_sync_param);
         if ( err != 0){
              printf("pthread_create error %s\n", strerror(errno));
              return 1;
