ec3e7b69c2a9898069eceee35ffd8ebb492b02b8
Jiandong Huang <huangjiandong@haloai.com>
2017-07-28 21:43:31 +0800

    tmp..

--- adb/file_sync_client.c
+++ adb/file_sync_client.c
@@ -1138,6 +1138,7 @@ int do_halo_pull(const char *rpath, const char *lpath, int show_progress, int co
         p_sync_param.rpath = rpath;
         p_sync_param.lpath = lpath;
         p_sync_param.show_progress = show_progress;
+        printf("======creating halo_sync_recv thread\n");
         if ((err = pthread_create(&t_recv, &attr, (void*)halo_sync_recv, (void*)&p_sync_param)) != 0){
              printf("pthread_create error %s\n", strerror(errno));
              return 1;
